ebe60dd73c2f7aa1bad433f3a2bb8eddb76bc29e
diovane.mendes <diovane.mendes@dbccompany.com.br>
2019-01-28 16:49:32 -0200

    :rocket: Configuração de inicialização da aplicação concluida

--- src/main/java/challenge/Application.java
+++ src/main/java/challenge/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -18,7 +19,7 @@ public class Application {
 	}
 
 	@Bean
-	public WebMvcProperties.LocaleResolver localeResolver(){
+	public LocaleResolver localeResolver(){
 		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
 		sessionLocaleResolver.setDefaultLocale(new Locale("pt", "BR"));
 		return sessionLocaleResolver;
